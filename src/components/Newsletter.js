c7d2ed5f1253e241496d92e556f41a726632c55d
mohdirfansheikh04 <mirfanbca@student.iul.ac.in>
2025-07-01 01:04:30 +0530

    newsletter update

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { Col, Row, Alert } from "react-bootstrap";
+import { useMediaQuery } from "react-responsive";
 
 export const Newsletter = () => {
-  const [email, setEmail] = useState('');
-  const [status, setStatus] = useState("");  // sending | error | success
+  const [email, setEmail] = useState("");
+  const [status, setStatus] = useState(""); // sending | error | success
   const [message, setMessage] = useState("");
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,11 +20,14 @@ export const Newsletter = () => {
 
     setStatus("sending");
     try {
-      const res = await fetch("https://portfoliobackend-std9.onrender.com/subscribe", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email }),
-      });
+      const res = await fetch(
+        "https://portfoliobackend-std9.onrender.com/subscribe",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email }),
+        }
+      );
 
       const data = await res.json();
 
@@ -44,22 +50,57 @@ export const Newsletter = () => {
       <div className="newsletter-bx wow slideInUp">
         <Row>
           <Col lg={12} md={6} xl={5}>
-            <h3>Subscribe to our Newsletter<br />& Never miss latest updates</h3>
-            {status === 'sending' && <Alert>Sending...</Alert>}
-            {status === 'error' && <Alert variant="danger">{message}</Alert>}
-            {status === 'success' && <Alert variant="success">{message}</Alert>}
+            <h3>
+              Subscribe to our Newsletter
+              <br />& Never miss latest updates
+            </h3>
+            {status === "sending" && <Alert>Sending...</Alert>}
+            {status === "error" && <Alert variant="danger">{message}</Alert>}
+            {status === "success" && <Alert variant="success">{message}</Alert>}
           </Col>
           <Col md={6} xl={7}>
             <form onSubmit={handleSubmit}>
-              <div className="new-email-bx">
+              {isMobile ? (
+                <div>
+                  <div className="new-email-bx">
+                    <input
+                      value={email}
+                      type="email"
+                      onChange={(e) => setEmail(e.target.value)}
+                      placeholder="Email Address"
+                    />
+                  </div>
+                  <button type="submit" className="btn btn-danger">
+                    Submit
+                  </button>
+                </div>
+              ) : (
+                <div>
+                  <div className="new-email-bx">
+                    <input
+                      value={email}
+                      type="email"
+                      onChange={(e) => setEmail(e.target.value)}
+                      placeholder="Email Address"
+                    />
+                    <button type="submit" className="btn btn-danger">
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              )}
+
+              {/* <div className="new-email-bx">
                 <input
                   value={email}
                   type="email"
                   onChange={(e) => setEmail(e.target.value)}
                   placeholder="Email Address"
                 />
-                <button type="submit">Submit</button>
               </div>
+              <button type="submit" className="btn btn-danger">
+                Submit
+              </button> */}
             </form>
           </Col>
         </Row>
